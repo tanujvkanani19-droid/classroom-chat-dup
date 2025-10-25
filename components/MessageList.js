5fbed5960837061ec71e983ab209301515ca915c
tanujvkanani19-droid <tanujvkanani19@gmail.com>
2025-10-25 11:35:01 +0530

    Update MessageList.js

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,15 +1,13 @@
 // components/MessageList.js
 import MessageItem from './MessageItem';
 
-// Remove 'messagesEndRef' from the props
-export default function MessageList({ messages }) {
+export default function MessageList({ messages, messagesEndRef }) {
   return (
     <div className="space-y-4">
       {messages.map((msg, index) => (
         <MessageItem key={index} user={msg.user} text={msg.text} />
       ))}
-
-      {/* We've removed the empty div with the ref */}
+      <div ref={messagesEndRef} />
     </div>
   );
 }
